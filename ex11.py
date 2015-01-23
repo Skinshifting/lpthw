1f70ce55d39cacd371e8b1ac2c8c91af1338cf15
Shalyn Noel <Skinshifting@users.noreply.github.com>
2015-01-23 15:37:17 -0600

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,12 @@
+# the comma prevents a new line
+# so the prompt for data shows on same line
+#you can put any kinda of data
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
